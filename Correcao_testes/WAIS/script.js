0654fb243023b1bb506ba44106bf058aad719b06
André Marques <111473714+TheBigLaw@users.noreply.github.com>
2026-03-23 10:45:14 -0300

    Update script.js

--- Correcao_testes/WAIS/script.js
+++ Correcao_testes/WAIS/script.js
@@ -774,9 +774,9 @@ async function baixarPDF() {
     await html2pdf().set({
       margin: [5, 5, 5, 5],
       filename: nomeArquivo,
-      image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 2, useCORS: true, logging: false, scrollY: 0 },
-      jsPDF: { unit: 'mm', format: [210, 2000], orientation: 'portrait' },
+      image: { type: 'jpeg', quality: 2.00 },
+      html2canvas: { scale: 1, useCORS: true, logging: false, scrollY: 0 },
+      jsPDF: { unit: 'mm', format: [210, 1500], orientation: 'portrait' },
       pagebreak: { mode: ['avoid-all'] }
     }).from(rel).save();
   } catch(e) {
